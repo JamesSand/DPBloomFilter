21c4748e1340806480077646bd370ca2d321e313
JamesSand <shazz20@mails.tsinghua.edu.cn>
2024-06-06 20:09:48 +0800

    ready to change output format and draw figs

--- scripts/run_dp.sh
+++ scripts/run_dp.sh
@@ -5,9 +5,15 @@ log_dir="logs"
 mkdir -p $log_dir
 
 # n_values=(1e3 5e3 1e4 5e4 1e5)
-eps_0_values=(0 5 10 15 20)
-m_exp_values=(16 20)
-n=5e4
+# eps_0_values=(0 5 10 15 20)
+eps_0_values=(1 2 3 4)
+# m_exp_values=(16 20)
+
+# m_exp_values=(16)
+# n=1e4
+
+m_exp_values=(20)
+n=1e5
 
 for m_exp in "${m_exp_values[@]}"; do
     for eps_0 in "${eps_0_values[@]}"; do
